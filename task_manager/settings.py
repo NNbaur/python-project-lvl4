43f1926ab0a25b4b09a98ae88feeccfe3b9023e5
BaurNN <studentsoftpu@gmail.com>
2022-07-17 09:30:05 +0300

    remake settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -16,6 +16,7 @@ from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,9 +134,3 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-import dj_database_url
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
