acb389922adc0d24138fd6dc094ec678c54e8c4e
BaurNN <studentsoftpu@gmail.com>
2022-07-17 12:48:43 +0300

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/NNbaur/python-project-lvl4/workflows/hexlet-check/badge.svg)](https://github.com/NNbaur/python-project-lvl4/actions)
+
+Task-Manager: https://rocky-plateau-42258.herokuapp.com/
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'webserver', 'rocky-plateau-42258.herokuapp.com', '0.0.0.0', ]
-
+ALLOWED_HOSTS = ['127.0.0.1', 'webserver', 'rocky-plateau-42258.herokuapp.com', ]
 
 # Application definition
 
